23e5cf27cc616a8d69d933dea3db5887b1344976
Aqsa Qayyum <aqsaqayyum@Mac-Aqsa-SE-104.local>
2021-11-05 23:25:34 +0500

    minor change

--- middelware/auth.js
+++ middelware/auth.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+var serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 async function auth(req, res, next) {
